6b4dd6c125e0b3dce6a3c4d45e5ed8c682a87921
ukengne <ulrich.kengne@student.uclouvain.be>
2024-12-14 17:10:42 +0100

    Proposition pour checkArchive

--- check_tar.c
+++ check_tar.c
@@ -22,8 +22,51 @@ tar_header_t header;
  */
 int check_archive(int tar_fd) {
     tar_header_t header;
-    int num_headers = 0;  
+    int num_headers = 0; 
+    unsigned int computed_chksum = 0; 
+
+    if (read(tar_fd, &header, sizeof(header)) != sizeof(header)){
+
+        printf("Un header doit faire 512 bytes\n");
+        return -5;
+    }
+
+    if (memcmp(header.magic,TMAGIC,TMAGLEN) !=0 || memcmp(header.magic,NULL,TMAGLEN) == 0){
+
+        printf("tu n'as pas le bon format\n");
+        return -1;
+    }
+
+    if (memcmp(header.version,TVERSION,TVERSLEN) !=0 || memcmp(header.version,NULL,TVERSLEN) == 0){
+
+        printf("tu n'as pas le bon format\n");
+        return -2;
+    }
+
+    unsigned int stored_chksum = TAR_INT(header.chksum);  // Somme attendue
+
+        char *raw_header = (char *)&header;
+
+
+    for (size_t i = 0; i < sizeof(header); i++) {
+        if (i >= 148 && i < 156) {
+            computed_chksum += ' '; 
+        } else {
+            computed_chksum += (unsigned char)raw_header[i];
+        }
+    }
+
+    if (computed_chksum != stored_chksum){
+        
+        return -3;
+    }
+
+    return 0;
+
+}
     
+    
+    /*
     while (read(tar_fd, &header, sizeof(header)) == sizeof(header)) {
         // Vérifiez si l'en-tête est vide (fin de l'archive)
         if (header.name[0] == '\0') {
@@ -66,3 +109,5 @@ int check_archive(int tar_fd) {
 
     return num_headers; 
 }
+
+*/
